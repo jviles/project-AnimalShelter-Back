6f2319eeb1a75005dd962ea8601c2d16bfce1f70
jviles <jvilesheras@hotmail.com>
2017-10-04 14:57:24 +0200

    shelters route

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var animals = require('./routes/animals');
+var shelters = require ('./routes/shelters');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 
 app.use('/animals', animals);
+app.use('/shelters',shelters);
 
 // catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- routes/animals.js
+++ routes/animals.js
@@ -4,11 +4,13 @@ var router = express.Router();
 var animals = [{
   id: 1,
   type: 'dog',
-  name: 'spot'
+  name: 'spot',
+  pictureUrl: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/Common-dog-behaviors-explained.jpg?itok=FSzwbBoi'
 }, {
-  id: 1,
+  id: 2,
   type: 'cat',
-  name: 'tiger'
+  name: 'tiger',
+  pictureUrl: 'http://www.petmd.com/sites/default/files/sleepy-cat-125522297.jpg'
 }];
 
 router.get('/', function(req, res) {
